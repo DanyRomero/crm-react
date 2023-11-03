f964510d9ba09e409aad086d649d1ea579d9fe8e
Dany Romero P <119898105+DanyRomeroP@users.noreply.github.com>
2023-11-03 13:19:17 -0600

    Add outlet router

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import NewClient from "./pages/NewClient";
+import Index from "./pages/Index";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<h1>Inicio</h1>
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, //esto significa que en el path / se renderiza el layout más el element asignado 
+        element: <Index /> // elemento que se renderiza en el outlet
+      },
+      {
+        path: "/clientes/nuevo",
+        element: <NewClient />,// elemento que se renderiza en el outlet
+      },
+      {
+      },
+    ],
   },
-  {
-    path:'*',
-    element:<h1>No existe esta página</h1>
-  }
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
